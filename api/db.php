765bd31418bccbebb1eb08b54673de58dea6cfbd
jaimerbernabela <jaimerbernabela@gmail.com>
2026-08-18 22:59:02 -0400

    Use buffered MySQL queries; native unbuffered prepares broke the sequential PREPARE/EXECUTE schema migration

--- api/db.php
+++ api/db.php
@@ -21,6 +21,7 @@ function fap_db(): PDO {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
         ]);
     }
     return $pdo;
